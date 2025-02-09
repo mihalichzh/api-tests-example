250cd97e2149b1ff282da7f5839a514004d0f033
Mikhail Zheltyshev <39348763+mihalichzh@users.noreply.github.com>
2025-02-09 14:11:26 +0100

    Add GitHub actions workflow (#1)
    Added base workflow example

--- src/core/service/todoist/todoist_service.py
+++ src/core/service/todoist/todoist_service.py
@@ -4,50 +4,58 @@ from typing import List
 from src.core.client.api_client import ApiClient, ClientConfig
 from src.core.service.base_service import BaseService
 from src.core.service.response_container import ResponseContainer
+from src.core.service.todoist.models.create_project.CreateProjectRequest import (
+    CreateProjectRequest,
+)
 from src.core.service.todoist.models.ProjectData import ProjectData
-from src.core.service.todoist.models.create_project.CreateProjectRequest import CreateProjectRequest
 
 
 class TodoistService(BaseService):
     def __init__(self, base_url: str, api_key: str = None):
-        super().__init__(
-            client=ApiClient(
-                config=ClientConfig(
-                    base_url=base_url
-                )
-            )
-        )
+        super().__init__(client=ApiClient(config=ClientConfig(base_url=base_url)))
         self.api_key = api_key
 
-    def create_new_project(self, body: CreateProjectRequest) -> ResponseContainer[ProjectData]:
+    def create_new_project(
+        self, body: CreateProjectRequest
+    ) -> ResponseContainer[ProjectData]:
         with self._context_step("create new project"):
             response = self.client.post(
                 path="/rest/v2/projects",
                 body=asdict(body),
-                headers={"Authorization": f"Bearer {self.api_key}"} if self.api_key else None
+                headers={"Authorization": f"Bearer {self.api_key}"}
+                if self.api_key
+                else None,
             )
             return ResponseContainer(
                 status_code=response.status_code,
                 headers=dict(**response.headers),
-                content=ProjectData(**response.json()) if response.ok else response.text,
+                content=ProjectData(**response.json())
+                if response.ok
+                else response.text,
             )
 
     def get_all_projects(self) -> ResponseContainer[List[ProjectData]]:
         with self._context_step("get all projects"):
             response = self.client.get(
                 path="/rest/v2/projects",
-                headers={"Authorization": f"Bearer {self.api_key}"} if self.api_key else None
+                headers={"Authorization": f"Bearer {self.api_key}"}
+                if self.api_key
+                else None,
             )
             return ResponseContainer(
                 status_code=response.status_code,
                 headers=dict(**response.headers),
-                content=[ProjectData(**item) for item in response.json()] if response.ok else response.text
+                content=[ProjectData(**item) for item in response.json()]
+                if response.ok
+                else response.text,
             )
 
     def delete_project(self, project_id: str) -> ResponseContainer[None]:
         response = self.client.delete(
             path=f"/rest/v2/projects/{project_id}",
-            headers={"Authorization": f"Bearer {self.api_key}"} if self.api_key else None
+            headers={"Authorization": f"Bearer {self.api_key}"}
+            if self.api_key
+            else None,
         )
         return ResponseContainer(
             status_code=response.status_code,
